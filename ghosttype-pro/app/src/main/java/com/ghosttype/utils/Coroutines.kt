cf7d63a09a0d3c56ded78c7789ac80c76d50f66c
tarefol Ajmal <42270143-tarefol251@users.noreply.replit.com>
2026-06-22 23:13:36 +0000

    cleanup: remove unused files (ServerGate, CrashGate, Hardener, LockScreen, BannerAdView, Coroutines, BrickedActivity, 7 unused drawables)

--- ghosttype-pro/app/src/main/java/com/ghosttype/utils/Coroutines.kt
+++
@@ -1,9 +0,0 @@
-package com.ghosttype.utils
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
-
-private val ioScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-fun runOnIO(block: suspend () -> Unit) { ioScope.launch { block() } }
